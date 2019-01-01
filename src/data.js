5f5be842a0a37222a3c49ea53c0da919f745e3a3
Rémi Thevenoux <remi.thevenoux@gmail.com>
2019-01-01 14:05:15 +0100

    Initial POC

---
+++ src/data.js
@@ -0,0 +1,34 @@
+var colors = {
+  "or": {
+    "label": "d'or",
+    "rgb": "ffff00"
+  },
+  "argent": {
+    "label": "d'argent",
+    "rgb": "ffffff"
+  },
+  "sable": {
+    "label": "de sable",
+    "rgb": "000000"
+  },
+  "azur": {
+    "label": "d'azur",
+    "rgb": "0000ff"
+  },
+  "sinople": {
+    "label": "de sinople",
+    "rgb": "00ff00"
+  },
+  "gueules": {
+    "label": "de gueules",
+    "rgb": "ff0000"
+  }
+}
+
+var forms = {
+  "old-french": { "label": "Français ancien", "shape": "M 300,660 C 300,660 600,550 600,260 L 600,0 L 0,0 L 0,260 C 0,550 300,660 300,660 z", "height": "660", "width": "600" },
+  "square": { "label": "Bannière", "shape": "M 0,0 L 0,0 0,500 L 500,500 L 500,0 z", "height": "500", "width": "500" },
+  //"oval": { "label": "", "shape": "" },
+  //"losange": { "label": "", "shape": "" },
+  //"spannish": { "label": "", "shape": "" }
+}
