7471ef4d18a7499cf2ae7e43fe81d8737901414a
Rémi Thevenoux <remi.thevenoux@gmail.com>
2019-01-08 16:43:38 +0100

    Add triangle-pattern and improve pattern-picker

--- src/gui/fillerPicker.js
+++ src/gui/fillerPicker.js
@@ -5,87 +5,43 @@ Vue.component('filler-picker', {
       plein: {
         color: 'azur'
       },
-      echiquete: {
+      pattern: {
         color1: 'azur',
         color2: 'or'
       },
-      losange: {
-        color1: 'gueules',
-        color2: 'argent'
-      },
       fusele: {
-        color1: 'sinople',
-        color2: 'argent',
-        angle: 'barre'
-      },
-      vair: {
-        color1: 'argent',
-        color2: 'azur'
-      },
-      contrevair: {
-        color1: 'argent',
-        color2: 'azur'
-      },
-      vair_en_pal: {
-        color1: 'argent',
-        color2: 'azur'
-      },
-      vair_en_pointe: {
-        color1: 'argent',
-        color2: 'azur'
-      },
+        angle: 'bande'
+      }
     }
   },
   computed: {
     value: {
       get: function () {
         switch (this.type) {
-          case "echiquete": return {
-            type: "pattern",
-            patternName: "echiquete",
-            color1: this.echiquete.color1,
-            color2: this.echiquete.color2
-          }
-          case "fusele": return {
-            type: "pattern",
-            patternName: "fusele",
-            angle: this.fusele.angle,
-            color1: this.fusele.color1,
-            color2: this.fusele.color2
-          }
-          case "losange": return {
-            type: "pattern",
-            patternName: "losange",
-            color1: this.losange.color1,
-            color2: this.losange.color2
-          }
           case "plein": return {
             type: "plein",
             color: this.plein.color,
           }
-          case "vair": return {
+
+          case "fusele": return {
             type: "pattern",
-            patternName: "vair",
-            color1: this.vair.color1,
-            color2: this.vair.color2
+            patternName: "fusele",
+            angle: this.fusele.angle,
+            color1: this.pattern.color1,
+            color2: this.pattern.color2
           }
-          case "contrevair": return {
-            type: "pattern",
-            patternName: "contrevair",
-            color1: this.contrevair.color1,
-            color2: this.contrevair.color2
-          }
-          case "vair_en_pal": return {
-            type: "pattern",
-            patternName: "vair_en_pal",
-            color1: this.vair_en_pal.color1,
-            color2: this.vair_en_pal.color2
-          }
+
+          case "echiquete":
+          case "losange":
+          case "triangle":
+          case "vair":
+          case "contrevair":
+          case "vair_en_pal":
           case "vair_en_pointe": return {
             type: "pattern",
-            patternName: "vair_en_pointe",
-            color1: this.vair_en_pointe.color1,
-            color2: this.vair_en_pointe.color2
+            patternName: this.type,
+            color1: this.pattern.color1,
+            color2: this.pattern.color2
           }
           default: return {
             type: this.type
@@ -105,56 +61,60 @@ Vue.component('filler-picker', {
   template: `
     <div>
       <div>
-        <label>Plein</label>
         <input type="radio" v-model="type" value="plein" @change="update">
+        <label>Plein</label>
         <color-picker v-model="plein.color" @input="update"></color-picker>
       </div>
-      <div>
-        <label>Échiqueté</label>
-        <input type="radio" v-model="type" value="echiquete" @change="update">
-        <color-picker v-model="echiquete.color1" @input="update"></color-picker>
-        <color-picker v-model="echiquete.color2" @input="update"></color-picker>
-      </div>
-      <div>
-        <label>Losangé</label>
-        <input type="radio" v-model="type" value="losange" @change="update">
-        <color-picker v-model="losange.color1" @input="update"></color-picker>
-        <color-picker v-model="losange.color2" @input="update"></color-picker>
-      </div>
-      <div>
-        <label>Fuselé</label>
-        <input type="radio" v-model="type" value="fusele" @change="update">
-        <color-picker v-model="fusele.color1" @input="update"></color-picker>
-        <color-picker v-model="fusele.color2" @input="update"></color-picker>
-        <select v-model="fusele.angle" @change="update">
-          <option value="defaut">défaut</option>
-          <option value="bande">en bande</option>
-          <option value="barre">en barre</option>
-        </select>
-      </div>
-      <div>
-        <label>Vair</label>
-        <input type="radio" v-model="type" value="vair" @change="update">
-        <color-picker v-model="vair.color1" @input="update"></color-picker>
-        <color-picker v-model="vair.color2" @input="update"></color-picker>
-      </div>
-      <div>
-        <label>Contre-vair</label>
-        <input type="radio" v-model="type" value="contrevair" @change="update">
-        <color-picker v-model="contrevair.color1" @input="update"></color-picker>
-        <color-picker v-model="contrevair.color2" @input="update"></color-picker>
-      </div>
-      <div>
-        <label>Vair en pal</label>
-        <input type="radio" v-model="type" value="vair_en_pal" @change="update">
-        <color-picker v-model="vair_en_pal.color1" @input="update"></color-picker>
-        <color-picker v-model="vair_en_pal.color2" @input="update"></color-picker>
-      </div>
-      <div>
-        <label>Vair en pointe</label>
-        <input type="radio" v-model="type" value="vair_en_pointe" @change="update">
-        <color-picker v-model="vair_en_pointe.color1" @input="update"></color-picker>
-        <color-picker v-model="vair_en_pointe.color2" @input="update"></color-picker>
+      <div class="flex-container" style="background-color:#D8D8D8">
+        <div style="flex-direction: column">
+          <div>
+            <input type="radio" v-model="type" value="echiquete" @change="update">
+            <label>Échiqueté</label>
+          </div>
+          <div>
+            <input type="radio" v-model="type" value="losange" @change="update">
+            <label>Losangé</label>
+          </div>
+          <div>
+            <input type="radio" v-model="type" value="triangle" @change="update">
+            <label>Trianglé</label>
+          </div>
+          <div>
+            <input type="radio" v-model="type" value="fusele" @change="update">
+            <label>Fuselé</label>
+            <select v-model="fusele.angle" @change="update">
+              <option value="defaut">défaut</option>
+              <option value="bande">en bande</option>
+              <option value="barre">en barre</option>
+            </select>
+          </div>
+        </div>
+        <div style="flex-direction: column">
+          <div>
+            <input type="radio" v-model="type" value="vair" @change="update">
+            <label>Vair</label>
+          </div>
+          <div>
+            <input type="radio" v-model="type" value="contrevair" @change="update">
+            <label>Contre-vair</label>
+          </div>
+          <div>
+            <input type="radio" v-model="type" value="vair_en_pal" @change="update">
+            <label>Vair en pal</label>
+          </div>
+          <div >
+            <input type="radio" v-model="type" value="vair_en_pointe" @change="update">
+            <label>Vair en pointe</label>
+          </div>
+        </div>
+        <div style="flex-direction: column">
+          <div>
+            <color-picker v-model="pattern.color1" @input="update"></color-picker>
+          <div>
+          </div>
+            <color-picker v-model="pattern.color2" @input="update"></color-picker>
+          <div>
+        </div>
       </div>
     </div>
     `
